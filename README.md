21c9bbc0bbbeda6dec65dd4af4fc9ca6c3e350de
iliasukhorukov <60256851+iliasukhorukov@users.noreply.github.com>
2020-03-05 20:01:41 +0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# generative-player
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,167 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var room1 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+var room2 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+
+function getTime(time) {
+  if (!isNaN(time)) {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    selectedTrack: null,
+    player: "stopped",
+    currentTime: null,
+    duration: null
+  };
+
+  componentDidMount() {
+    this.player.addEventListener("timeupdate", e => {
+      this.setState({
+        currentTime: e.target.currentTime,
+        duration: e.target.duration
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.player.removeEventListener("timeupdate", () => {});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.selectedTrack !== prevState.selectedTrack) {
+      let track;
+      switch (this.state.selectedTrack) {
+        case "1st room":
+          track = room1;
+          break;
+        case "2nd room":
+          track = room2;
+          break;
+        default:
+          break;
+      }
+      if (track) {
+        this.player.src = track;
+        this.player.play();
+        this.setState({ player: "playing", duration: this.player.duration });
+      }
+    }
+    if (this.state.player !== prevState.player) {
+      if (this.state.player === "paused") {
+        this.player.pause();
+      } else if (this.state.player === "stopped") {
+        this.player.pause();
+        this.player.currentTime = 0;
+        this.setState({ selectedTrack: null });
+      } else if (
+        this.state.player === "playing" &&
+        prevState.player === "paused"
+      ) {
+        this.player.play();
+      }
+    }
+  }
+
+  
+
+  render() {
+    const list = [
+      { id: 1, title: "1st room" },
+      { id: 2, title: "2nd room" }
+    ].map(item => {
+      return (
+        <li
+          key={item.id}
+          onClick={() => this.setState({ selectedTrack: item.title })}
+        >
+          {item.title}
+        </li>
+      );
+    });
+
+    const currentTime = getTime(this.state.currentTime);
+    const duration = getTime(this.state.duration);
+
+    return (
+      <>
+        <h1>Generative Player</h1>
+        <ul>{list}</ul>
+        <div>
+          {this.state.player === "paused" && (
+            <button onClick={() => this.setState({ player: "playing" })}>
+              Play
+            </button>
+          )}
+          {this.state.player === "playing" && (
+            <button onClick={() => this.setState({ player: "paused" })}>
+              Pause
+            </button>
+          )}
+          {this.state.player === "playing" || this.state.player === "paused" ? (
+            <button onClick={() => this.setState({ player: "stopped" })}>
+              Stop
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+        {this.state.player === "playing" || this.state.player === "paused" ? (
+          <div>
+            {currentTime} / {duration}
+          </div>
+        ) : (
+          ""
+        )}
+        <audio ref={ref => (this.player = ref)} />
+      </>
+    );
+  }
+}
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A path was submitted: ' + this.state.value);
+    room1 = this.state.value;
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Filepath:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+const inputElement1 = document.getElementById("path1");
+const inputElement2 = document.getElementById("path2");
+
+ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<NameForm />, inputElement1);
+ReactDOM.render(<NameForm />, inputElement2);
+
